1e65c04fcc6f93ee0eba5c0be2f94842cfb341c2
Theodlz <theophile.dulaz@gmail.com>
2023-10-26 08:58:08 -0700

    save upload error message to DB

--- nmma_api/services/retrieval_queue.py
+++ nmma_api/services/retrieval_queue.py
@@ -19,15 +19,51 @@ def retrieval_queue():
     while True:
         try:
             # get the analysis requests that have been processed
-            analysis_requests = mongo.db.analysis.find({"status": "running"})
+            analysis_requests = mongo.db.analysis.find(
+                {"status": {"$in": ["running", "failed_upload"]}}
+            )
             for analysis in analysis_requests:
-                results = retrieve(analysis)
-                if results is not None:
-                    mongo.db.analysis.update_one(
-                        {"_id": analysis["_id"]},
-                        {"$set": {"status": "complete"}},
+                if (
+                    analysis["status"] == "failed_upload"
+                    and analysis.get("nb_upload_failures", 0) >= 10
+                ):
+                    log(
+                        f"Analysis {analysis['_id']} has failed to upload 10 times. Skipping."
                     )
-                    upload_analysis_results(results, analysis)
+                    continue
+                if analysis["status"] == "running":
+                    results = retrieve(analysis)
+                else:
+                    try:
+                        results = mongo.db.results.find_one(
+                            {"analysis_id": analysis["_id"]}
+                        )["results"]
+                    except Exception:
+                        results = retrieve(analysis)
+                if results is not None:
+                    if analysis["status"] != "failed_upload":
+                        mongo.insert_one(
+                            "results",
+                            {"analysis_id": analysis["_id"], "results": results},
+                        )
+                    uploaded, error = upload_analysis_results(results, analysis)
+                    if uploaded:
+                        mongo.db.analysis.update_one(
+                            {"_id": analysis["_id"]},
+                            {"$set": {"status": "completed"}},
+                        )
+                    else:
+                        mongo.db.analysis.update_one(
+                            {"_id": analysis["_id"]},
+                            {
+                                "$set": {
+                                    "status": "failed_upload",
+                                    "nb_upload_failures": analysis["nb_upload_failures"]
+                                    + 1,
+                                    "upload_error": error,
+                                }
+                            },
+                        )
         except Exception as e:
             log(f"Failed to retrieve analysis results from expanse: {e}")
 
